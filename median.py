fcf198de30a9903316fcf15fe730e437dc697794
RoaaKhalil <99557821+RoaaKhalil@users.noreply.github.com>
2022-10-02 14:29:43 +0100

    update median.py
    correct the errors

--- median.py
+++ median.py
@@ -15,7 +15,7 @@ listLeng = len(numbers)
 numbersorted = numbers.sort()
 
 if (listLeng % 2 !=0 ):
-    return numbersorted[listleng // 2 ]
+    print(numbersorted[listleng // 2 ])
 else:
     med = (numbersorted[listleng // 2 ] + numbersorted[listleng // 2 + 1 ]) / 2
-    return med
+    print( med)
